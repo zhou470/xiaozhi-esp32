630d8c3fdc005f8ddc8f935e5ffe95089c8127e3
zhou470 <957969614@QQ.COM>
2026-06-12 17:21:40 +0800

    Update lichuang_c3_dev_board.cc

--- main/boards/lichuang-c3-dev/lichuang_c3_dev_board.cc
+++ main/boards/lichuang-c3-dev/lichuang_c3_dev_board.cc
@@ -1,74 +1,19 @@
-#include "wifi_board.h"
-#include "codecs/es8311_audio_codec.h"
-#include "display/display.h"
-#include "application.h"
-#include "button.h"
-#include "config.h"
-#include "i2c_device.h"
-
-#include <esp_log.h>
-#include <driver/i2c_master.h>
-
-#define TAG "LichuangC3DevBoard"
-
-class LichuangC3DevBoard : public WifiBoard {
-private:
-    i2c_master_bus_handle_t codec_i2c_bus_;
-    Button boot_button_;
-
-    void InitializeI2c() {
-        i2c_master_bus_config_t i2c_bus_cfg = {
-            .i2c_port = I2C_NUM_0,
-            .sda_io_num = AUDIO_CODEC_I2C_SDA_PIN,
-            .scl_io_num = AUDIO_CODEC_I2C_SCL_PIN,
-            .clk_source = I2C_CLK_SRC_DEFAULT,
-            .glitch_ignore_cnt = 7,
-            .intr_priority = 0,
-            .trans_queue_depth = 0,
-            .flags = {
-                .enable_internal_pullup = 1,
-            },
-        };
-        ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &codec_i2c_bus_));
-    }
-
-    void InitializeButtons() {
-        boot_button_.OnClick([this]() {
-            auto& app = Application::GetInstance();
-            if (app.GetDeviceState() == kDeviceStateStarting) {
-                EnterWifiConfigMode();
-                return;
-            }
-            app.ToggleChatState();
-        });
-    }
-
+class Pca9557 : public I2cDevice {
 public:
-    LichuangC3DevBoard() : boot_button_(BOOT_BUTTON_GPIO) {
-        InitializeI2c();
-        InitializeButtons();
+    Pca9557(i2c_master_bus_handle_t i2c_bus, uint8_t addr) : I2cDevice(i2c_bus, addr) {
+        ESP_LOGI("PCA9557", "Init at 0x%02X", addr);
+        esp_err_t err = i2c_master_probe(i2c_bus, addr, 100);
+        if (err != ESP_OK) {
+            ESP_LOGW("PCA9557", "Device not found at 0x%02X, skipping", addr);
+            return;
+        }
+        ESP_ERROR_CHECK_WITHOUT_ABORT(i2c_master_transmit(i2c_device_, (uint8_t[]){0x01, 0x03}, 2, 100));
+        ESP_ERROR_CHECK_WITHOUT_ABORT(i2c_master_transmit(i2c_device_, (uint8_t[]){0x03, 0xf8}, 2, 100));
     }
 
-    virtual AudioCodec* GetAudioCodec() override {
-        static Es8311AudioCodec audio_codec(
-            codec_i2c_bus_, 
-            I2C_NUM_0, 
-            AUDIO_INPUT_SAMPLE_RATE, 
-            AUDIO_OUTPUT_SAMPLE_RATE,
-            AUDIO_I2S_GPIO_MCLK, 
-            AUDIO_I2S_GPIO_BCLK, 
-            AUDIO_I2S_GPIO_WS, 
-            AUDIO_I2S_GPIO_DOUT, 
-            AUDIO_I2S_GPIO_DIN,
-            GPIO_NUM_NC, 
-            AUDIO_CODEC_ES8311_ADDR);
-        return &audio_codec;
-    }
-
-    virtual Display* GetDisplay() override {
-        static NoDisplay display;
-        return &display;
+    void SetOutputState(uint8_t bit, uint8_t level) {
+        uint8_t data = ReadReg(0x01);
+        data = (data & ~(1 << bit)) | (level << bit);
+        WriteReg(0x01, data);
     }
 };
-
-DECLARE_BOARD(LichuangC3DevBoard);
