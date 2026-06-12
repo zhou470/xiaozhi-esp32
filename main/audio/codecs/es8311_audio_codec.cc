db66996156ffa4a890086d01938199ba3c213012
zhou470 <957969614@QQ.COM>
2026-06-12 14:32:19 +0800

    Update es8311_audio_codec.cc

--- main/audio/codecs/es8311_audio_codec.cc
+++ main/audio/codecs/es8311_audio_codec.cc
@@ -53,7 +53,10 @@ Es8311AudioCodec::Es8311AudioCodec(void* i2c_master_handle, i2c_port_t i2c_port,
 
     if (codec_if_ == nullptr) {
         ESP_LOGE(TAG, "Failed to create Es8311AudioCodec - running without audio");
-    return;
+            duplex_ = false;
+        output_enabled_ = false;
+        input_enabled_ = false;
+        return;
     } else {
         ESP_LOGI(TAG, "Es8311AudioCodec initialized");
     }
