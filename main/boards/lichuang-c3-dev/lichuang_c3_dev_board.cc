f5a98d99d3cf1e1a8a41c097c92a38bfb9983f60
zhou470 <957969614@QQ.COM>
2026-06-12 17:22:46 +0800

    Update lichuang_c3_dev_board.cc

--- main/boards/lichuang-c3-dev/lichuang_c3_dev_board.cc
+++ main/boards/lichuang-c3-dev/lichuang_c3_dev_board.cc
@@ -1,3 +1,16 @@
+#include "wifi_board.h"
+#include "codecs/es8311_audio_codec.h"
+#include "display/display.h"
+#include "application.h"
+#include "button.h"
+#include "config.h"
+#include "i2c_device.h"
+
+#include <esp_log.h>
+#include <driver/i2c_master.h>
+
+#define TAG "LichuangC3DevBoard"
+
 class Pca9557 : public I2cDevice {
 public:
     Pca9557(i2c_master_bus_handle_t i2c_bus, uint8_t addr) : I2cDevice(i2c_bus, addr) {
@@ -17,3 +30,67 @@ public:
         WriteReg(0x01, data);
     }
 };
+
+class LichuangC3DevBoard : public WifiBoard {
+private:
+    i2c_master_bus_handle_t codec_i2c_bus_;
+    Button boot_button_;
+    Pca9557* pca9557_;
+
+    void InitializeI2c() {
+        i2c_master_bus_config_t i2c_bus_cfg = {
+            .i2c_port = I2C_NUM_0,
+            .sda_io_num = AUDIO_CODEC_I2C_SDA_PIN,
+            .scl_io_num = AUDIO_CODEC_I2C_SCL_PIN,
+            .clk_source = I2C_CLK_SRC_DEFAULT,
+            .glitch_ignore_cnt = 7,
+            .intr_priority = 0,
+            .trans_queue_depth = 0,
+            .flags = {
+                .enable_internal_pullup = 1,
+            },
+        };
+        ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &codec_i2c_bus_));
+        pca9557_ = new Pca9557(codec_i2c_bus_, 0x1F);
+    }
+
+    void InitializeButtons() {
+        boot_button_.OnClick([this]() {
+            auto& app = Application::GetInstance();
+            if (app.GetDeviceState() == kDeviceStateStarting) {
+                EnterWifiConfigMode();
+                return;
+            }
+            app.ToggleChatState();
+        });
+    }
+
+public:
+    LichuangC3DevBoard() : boot_button_(BOOT_BUTTON_GPIO) {
+        InitializeI2c();
+        InitializeButtons();
+    }
+
+    virtual AudioCodec* GetAudioCodec() override {
+        static Es8311AudioCodec audio_codec(
+            codec_i2c_bus_, 
+            I2C_NUM_0, 
+            AUDIO_INPUT_SAMPLE_RATE, 
+            AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_GPIO_MCLK, 
+            AUDIO_I2S_GPIO_BCLK, 
+            AUDIO_I2S_GPIO_WS, 
+            AUDIO_I2S_GPIO_DOUT, 
+            AUDIO_I2S_GPIO_DIN,
+            GPIO_NUM_NC, 
+            AUDIO_CODEC_ES8311_ADDR);
+        return &audio_codec;
+    }
+
+    virtual Display* GetDisplay() override {
+        static NoDisplay display;
+        return &display;
+    }
+};
+
+DECLARE_BOARD(LichuangC3DevBoard);
